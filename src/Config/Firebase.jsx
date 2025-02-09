89a5deb708a551e2bbce1175cc2ab5289581766b
Adebimpe Favour <67767150+Proton-number@users.noreply.github.com>
2025-02-09 06:22:05 +0100

    Update Firebase.jsx
    Added config keys from firebase

--- src/Config/Firebase.jsx
+++ src/Config/Firebase.jsx
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDLxA_omGbvtsNe6uY-zgdJtUe2wEZWSMI",
+  authDomain: "valentine-20d2a.firebaseapp.com",
+  projectId: "valentine-20d2a",
+  storageBucket: "valentine-20d2a.firebasestorage.app",
+  messagingSenderId: "636013255673",
+  appId: "1:636013255673:web:dece9f63d9fd616da9bb28",
+  measurementId: "G-N18ZQRCVBZ",
 };
 
 // Initialize Firebase
